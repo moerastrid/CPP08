d1ed634ba41d5f6d95f25f0fe74196b1f1cfbb6f
astrid <moerastrid@gmail.com>
2023-08-28 13:39:10 +0200

    start ex00

---
+++ ex00/main.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ageels <ageels@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/08/28 11:21:07 by ageels        #+#    #+#                 */
+/*   Updated: 2023/08/28 13:22:57 by ageels        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "easyfind.hpp"
+#include <iostream>
+#include <vector>
+#include <array>
+
+int main() {
+	int number = 1;
+
+	std::vector<int> myvector;
+	std::vector<int> emptyvector;
+	for (int i(2); i<=4; i++) {
+		myvector.push_back(i);
+	}
+
+	std::array<int, 5> myarray = {0, 1, 2, 3, 4};
+
+	easyfind(myvector, number);
+	easyfind(myarray, number);
+	easyfind(emptyvector, number);
+	//std::cout << "vector " << easyfind(myvector, number).begin() << std::endl;
+	//std::cout << "number " << easyfind(myarray, number).begin() << std::endl;
+	
+	std::cout << " - - - THE END - - - " << std::endl;
+	return (0);
+}
